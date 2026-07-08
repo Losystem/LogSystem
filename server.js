923c2f69fad1df11860b7aca4770c9117dc58980
yannickfoning <yannickfoning22@gmail.com>
2026-07-08 01:44:56 +0100

    Remove Express body size limits to allow unlimited file imports

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ app.use(compression({
     return compression.filter(req, res);
   }
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
